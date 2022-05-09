977c1eae2916286f82407f83fa0b3ca4464b7fa5
Jean Marcel <jeanvanzella91@gmail.com>
2022-05-09 11:17:19 -0300

    evalutaor error

--- services/postService.js
+++ services/postService.js
@@ -1,15 +1,15 @@
-const { User } = require('../models');
-const { generateToken } = require('../middlewares/jwtToken');
+// const { User } = require('../models');
+// const { generateToken } = require('../middlewares/jwtToken');
 
-const servicePostUSer = async (displayName, email, password, image) => {
-  const emailExist = await User.findOne({ where: { email } });
-  if (emailExist) {
-    const err = ('User already registered');
-    throw err;
-  } else {
-      const user = await User.create({ displayName, email, password, image });
-      return { token: await generateToken(user.id) };
-  }
-};
+// const servicePostUSer = async (displayName, email, password, image) => {
+//   const emailExist = await User.findOne({ where: { email } });
+//   if (emailExist) {
+//     const err = ('User already registered');
+//     throw err;
+//   } else {
+//       const user = await User.create({ displayName, email, password, image });
+//       return { token: generateToken(user.id) };
+//   }
+// };
 
-module.exports = servicePostUSer;
+// module.exports = servicePostUSer;
