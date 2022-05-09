d85a4bd29c6103283665dc5f34e29c35e6491809
Jean Marcel <jeanvanzella91@gmail.com>
2022-05-09 02:43:47 -0300

    problem on model index, had to restart

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const { Users } = require('./models'); 
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.get('/users', async (_req, res) => {
+  try {
+  const users = await Users.findAll();
+  
+  return res.status(200).json(users);
+  } catch (e) {
+  console.log(e.message);
+  res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+  });
---
+++ models/users.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('Users', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, { foreignKey: 'userId', as: 'blogPosts' }); 
+};
+
+  return User;
+};
