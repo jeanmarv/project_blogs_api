180a4e4e518b852d1c6beaf8dfb597615dd37f7f
Jean Marcel <jeanvanzella91@gmail.com>
2022-05-10 10:14:06 -0300

    fixing req 9

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,20 +14,18 @@ const getPost = async (req, res) => {
 const getById = async (req, res) => {
   try {
     const { id } = req.params;
-    const userInfo = ['id', 'displayName', 'email', 'image'];
-    const getPostById = await BlogPosts.findOne({
-    where: { id },
-    include: [
-      { model: Users, as: 'user', attributes: userInfo },
-      { model: Categories, as: 'categories', through: { attributes: [] } },
-    ],
-  });
-  if (!getPostById) {
-    res.status(404).json({ message: 'Post does not exist' });
-  }
-  return res.status(200).json(getPostById);
-  } catch (error) {
-    res.status(401).json({ message: error.message });
+    const getPostById = await BlogPosts.findByPk(id, {
+      include: [
+        { model: Users, as: 'user', attributes: { exclude: ['password'] } },
+        { model: Categories, as: 'categories', through: { attributes: [] } },
+      ],
+    });
+    if (!getPostById) {
+      return res.status(404).json({ message: 'Post does not exist' });
+    }
+    return res.status(200).json(getPostById);
+  } catch (err) {
+    res.status(401).json({ message: err.message });
   }
 };
 
